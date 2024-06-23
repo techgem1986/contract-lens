5dfd7e4282deac215e5a696306c84a34620a4e56
Sandipan Sarkar <work.sandipan@gmail.com>
2024-06-23 13:52:00 +0530

    Initial Commit

--- python/app.py
+++ python/app.py
@@ -1,6 +1,7 @@
 import boto3
 from fastapi import FastAPI
 import uvicorn
+import json
 
 app = FastAPI()
 
@@ -10,12 +11,17 @@ async def root():
     return {"message": "Welcome to Contract Lens"}
 
 
-@app.get("/sagemaker_endpoint")
-async def sagemaker_endpoint():
-    session = boto3.Session()
-    sagemaker_client = session.client("sagemaker")
-    response = sagemaker_client.list_endpoints()
-    return response
+@app.get("/predict/")
+async def sagemaker_endpoint(prompt:str = None):
+    session = boto3.Session(profile_name="default")
+    runtime = session.client("runtime.sagemaker")
+    llm_endpoint="mistral-llm"
+    if prompt is not None:
+        response = runtime.invoke_endpoint(EndpointName=llm_endpoint, ContentType="application/json", Body=json.dumps({"inputs":f"<s>{prompt}[/INST]"}).encode("utf-8"))
+        return json.loads(response['Body'].read().decode('utf-8'))
+    else:
+        return "No prompt provided"
+
 
 
 if __name__ == "__main__":
