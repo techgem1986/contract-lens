bb4f2fc45901a3826572e736a0c1bc62d73b451e
barunkumar04 <barun.kumar04@gmail.com>
2024-06-27 15:18:08 +0530

    Contract compare changes

--- python/app.py
+++ python/app.py
@@ -6,6 +6,21 @@ import os
 from loader import load_documents
 from llm import get_conversation_chain, get_llm_chain
 from typing import List
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+from transformers import AutoTokenizer, GenerationConfig, AutoModelForCausalLM
+from langchain_community.llms import HuggingFacePipeline
+from langchain_community.llms import SagemakerEndpoint
+from langchain_community.llms.sagemaker_endpoint import LLMContentHandler
+from typing import Dict
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+
+
+import warnings
+warnings.filterwarnings('ignore')
 
 app = FastAPI()
 session = boto3.Session(profile_name="default")
@@ -41,11 +56,81 @@ async def search(prompt=None, region=None, sector=None):
         return "Please provide a prompt"
 
 
-@app.post("/compare/")
+@app.get("/compare/")
 async def compare(files: List[UploadFile] = File(...)):
+
+    docs = []
     for file in files:
-        print(file.filename)
-    return "Functionality to be implemented"
+        print("loading file - "+ file.filename)
+        docs.extend(PyPDFLoader(file).load())
+
+    text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1500,
+    chunk_overlap = 150
+    )
+    splits = text_splitter.split_documents(docs)
+
+    embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vector_db = FAISS.from_documents(documents=splits, embedding=embedding)
+
+    class ContentHandler(LLMContentHandler):
+        content_type = "application/json"
+        accepts = "application/json"
+
+        def transform_input(self, prompt: str, model_kwargs: Dict) -> bytes:
+            input_str = json.dumps({"inputs": prompt, "parameters": model_kwargs})
+            return input_str.encode("utf-8")
+
+        def transform_output(self, output: bytes) -> str:
+            response_json = json.loads(output.read().decode("utf-8"))
+            return response_json[0]["generated_text"]
+
+
+    content_handler = ContentHandler()
+
+    runtime = boto3.client("runtime.sagemaker")
+
+    llm=SagemakerEndpoint(
+        endpoint_name="mistral-llm",
+        client=runtime,
+        model_kwargs={"temperature": 0.5, "max_new_tokens":750},
+        content_handler=content_handler
+    )
+
+    template = """
+    You are master in comparing two PDFs. 
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details.
+    If the answer is not in provided context just say, "Answer is not available in the context.", please don't provide the wrong answer.
+
+    Provide Confidence Score as well.
+
+    Context:\n {context}?\n
+    Question: \nCompare and list out similarities and differences of contracts based on {question}.\n
+
+    Confidence Score:
+    """
+
+    prompt = PromptTemplate(template = template, input_variables = ["context", "question"])
+
+    chain = RetrievalQA.from_chain_type(
+        llm = llm,
+        retriever = vector_db.as_retriever(search_kwargs={"k":12}),
+        return_source_documents=False,
+        chain_type = "stuff",
+        chain_type_kwargs={"prompt":prompt}
+    )
+
+
+    question = "record dates"
+    response = chain({"query":question})
+
+    response_string = response.get("result")
+
+
+    sub = "Question:"
+    output = output + response_string.rsplit(sub, 1)[1]
+
+    return output
 
 
 @app.get("/chat/")
